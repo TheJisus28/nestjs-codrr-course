b860ac6b7a22902c53094e6d25d99c8f2784f9c6
TheJisus28 <jesus.carrascalh@gmail.com>
2025-04-19 22:29:18 -0500

    feat: configure TypeORM with PostgreSQL and add Projects module

---
+++ src/config/data.source.ts
@@ -0,0 +1,25 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+void ConfigModule.forRoot({
+  envFilePath: `.env.${process.env.NODE_ENV}`,
+});
+
+const configService = new ConfigService();
+
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../../migrations/**/*{.ts,.js}'],
+  synchronize: false,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const AppDS = new DataSource(DataSourceConfig);
