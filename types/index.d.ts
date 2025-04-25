1c0e4460a94facfadf0d53ab6733fa2f620fec7b
TheJisus28 <jesus.carrascalh@gmail.com>
2025-04-25 08:52:54 -0500

    feat: add HASH_SALT to environment variables and update type definitions; include @types/bcrypt in dependencies

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_NAME: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    HASH_SALT: string;
+  }
+}
