ab92df65a32860a2c2821ca1c33ff3793c6b01f6
TheJisus28 <jesus.carrascalh@gmail.com>
2025-05-01 21:25:01 -0500

    feat: add guards and access level decorator to TasksController for enhanced security

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { TasksService } from '../services/tasks.service';
 import { TaskDTO } from '../dto/task.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { AccessLevelGuard } from 'src/auth/guards/access-level.guard';
+import { AccessLevel } from 'src/auth/decorators/access-level.decorator';
+import { ACCESS_LEVEL } from 'src/constants/roles';
 
 @Controller('tasks')
+@UseGuards(AuthGuard, RolesGuard, AccessLevelGuard)
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
+  @AccessLevel(ACCESS_LEVEL.DEVELOPER)
   @Post('create/projectId/:projectId')
   public async createTask(
     @Body() body: TaskDTO,
