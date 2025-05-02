663e57bc545d0b11f9185679cbcec7fd200657ea
TheJisus28 <jesus.carrascalh@gmail.com>
2025-05-01 21:20:49 -0500

    feat: implement createTask method in TasksController for task creation

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { TasksService } from '../services/tasks.service';
+import { TaskDTO } from '../dto/task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post('create/projectId/:projectId')
+  public async createTask(
+    @Body() body: TaskDTO,
+    @Param('projectId') projectId: string,
+  ) {
+    return await this.tasksService.createTask(body, projectId);
+  }
+}
